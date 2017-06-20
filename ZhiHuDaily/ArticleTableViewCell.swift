3e34fd59c249581df66d3d4ab4adf39e2fef097e
Vivian <1354458047@qq.com>
2017-06-20 11:25:30 +0800

    remove commented codes

--- ZhiHuDaily/ArticleTableViewCell.swift
+++ ZhiHuDaily/ArticleTableViewCell.swift
@@ -14,8 +14,6 @@ class ArticleTableViewCell: UITableViewCell {
     @IBOutlet weak var myImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-      
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -28,27 +26,8 @@ class ArticleTableViewCell: UITableViewCell {
 //        self.myImageView?.frame = CGRect(x: 10,y: 0,width: 120,height: 100)
         self.myImageView?.contentMode = UIViewContentMode.scaleAspectFit
         self.myImageView?.clipsToBounds = true
-//
+        
 //        self.myTextLabel?.frame = CGRect(x: 140,y: 0,width: self.frame.width-150,height: 100)
         self.myTextLabel.numberOfLines = 2
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(didChangeOrientation), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
     }
-    
-    //监测方向变化
-    func didChangeOrientation() {
-//        if (UIDevice.current.orientation == .portrait) {
-//            UIView.animate(withDuration: 0, animations: {
-//                self.contentView.transform = .identity
-//                self.contentView.bounds = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-//            })
-//        } else {
-//            UIView.animate(withDuration: 0, animations: {
-//                //                self.view.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_2));
-//                self.contentView.bounds = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.height, height: UIScreen.main.bounds.width)
-//            })
-//        }
-//        layoutSubviews()
-    }
-    
 }
